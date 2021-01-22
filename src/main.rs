3f9d719ec00ed3f3b23a5da187262c0db40adbc6
hyperchessbot <hyperchessbot@gmail.com>
2021-01-22 18:20:33 +0000

    Basic routing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder, HttpResponse};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    format!("Hello!")
+}
+
+#[get("/{name}")]
+async fn welcome(web::Path(name): web::Path<String>) -> impl Responder {
+    format!("Hello {}!", name)
+}
+
+#[get("/{greeting}/{name}")]
+async fn greeting(web::Path((greeting, name)): web::Path<(String, String)>) -> impl Responder {
+    format!("{} {}!", greeting, name)
+}
+
+async fn page() -> impl Responder {
+    HttpResponse::Ok().body("Page!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new()
+        .service(index)
+        .service(welcome)
+        .service(greeting)
+        .service(
+            actix_web::web::resource(vec!["/page"])
+                .route(actix_web::web::get().to(page))
+                .route(actix_web::web::post().to(page))
+        )
+    )
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
