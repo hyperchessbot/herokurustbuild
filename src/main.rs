f32d971afbf172fd2cc837fdab889caaf96c8dee
hyperchessbot <hyperchessbot@gmail.com>
2021-01-26 12:10:09 +0000

    Run bot detached

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,49 @@
+use actix_web::{get, App, HttpServer, web, Responder};
+
 use log::{log_enabled, info, Level};
 
 extern crate env_logger;
 
-use actix_web::{get, App, HttpServer, web, Responder};
-
 use lichessbot::lichessbot::*;
 
-fn main() -> std::io::Result<()> {
+#[actix_web::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
     env_logger::init();
-
+    
     let port = std::env::var("PORT").unwrap_or("8080".to_string());
 
-    let bot = LichessBot::new().enable_casual(true);
-
+    let bot = Box::leak(Box::new(LichessBot::new().enable_casual(true)));
+    
     let bot_data = web::Data::new(bot.state.clone());
-
-    let tokio_rt = tokio::runtime::Builder::new_current_thread()
-        .enable_all()
-        .build()?;
-
-    tokio_rt.spawn(async move {
-        let mut bot = bot;
-
+    
+    let spawn_result = tokio::spawn(async move {
         if log_enabled!(Level::Info){
-            info!("starting bot {:?}", std::env::var("RUST_BOT_NAME"));
+            info!("starting bot stream");
         }
 
-        let _ = bot.stream().await;
-    });
+        bot.stream().await
+    }).await;
 
-    let server = actix_web::rt::System::new("abc").block_on(async {
-        let server = HttpServer::new(move || App::new()
-            .app_data(bot_data.clone())
-            .service(index)
-            )            
-            .disable_signals()            
-            .bind(format!("0.0.0.0:{}", port))?
-            .run();
+    HttpServer::new(move || App::new()
+        .app_data(bot_data.clone())
+        .service(index)
+    )            
+    .disable_signals()            
+    .bind(format!("0.0.0.0:{}", port))?
+    .run();
 
-        Ok::<_, std::io::Error>(server)
-    })?;
+    let _ = tokio::signal::ctrl_c().await;
 
-     tokio_rt.block_on(async move {
-        let _ = tokio::signal::ctrl_c().await;
-        let _ = server.stop(true).await;
-        Ok(())
-    })
+    if let Ok((tx, mut rxa)) = spawn_result {
+        let _ = tx.send("stopped by user".to_string()).await;
+
+	    println!("{:?}", rxa.recv().await);
+    }
+	
+	Ok(())
 }
-
 
 #[get("/")]
 async fn index(bot_state: web::Data::<std::sync::Arc<tokio::sync::Mutex<BotState>>>) -> impl Responder {
-    format!("Hello! {:?}", bot_state)
+    format!("Bot state : {:?}", bot_state)
 }
