b110c5cccaa83348b188ed2c934142cffcc4dbcb
hyperchessbot <73081047+hyperchessbot@users.noreply.github.com>
2021-02-20 14:42:20 +0100

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -3,7 +3,8 @@
 # download engine
 # wget https://s3-us-west-2.amazonaws.com/variant-stockfish/ddugovic/master/stockfish-x86_64 -O stockfish12
 # wget "https://github.com/pythonideas/binrepo/blob/main/Stockfish13/stockfish_13_linux_x64?raw=true" -O stockfish12
-wget "https://github.com/pythonideas/binrepo/blob/main/Stockfish13/stockfish_13_linux_x64_bmi2?raw=true" -O stockfish12
+# wget "https://github.com/pythonideas/binrepo/blob/main/Stockfish13/stockfish_13_linux_x64_bmi2?raw=true" -O stockfish12
+wget "https://github.com/pythonideas/binrepo/blob/main/Stockfish13/stockfish_13_linux_x64_avx2?raw=true" -O stockfish12
 chmod +x stockfish12
 
 # download book
